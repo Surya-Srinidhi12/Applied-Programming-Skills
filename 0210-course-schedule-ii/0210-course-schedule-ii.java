6bd9c115d767f28f372400f9f9a2d09a0b45f132
Surya-Srinidhi12 <vtu24957@veltech.edu.in>
2026-04-28 11:41:41 +0530

    Time: 5 ms (83.88%), Space: 46.2 MB (99.27%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> graph = new ArrayList<>();
+        for(int i = 0; i < numCourses; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        int[] indegree = new int[numCourses];
+
+        for(int[] p : prerequisites) {
+            graph.get(p[1]).add(p[0]);
+            indegree[p[0]]++;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i = 0; i < numCourses; i++) {
+            if(indegree[i] == 0) {
+                queue.add(i);
+            }
+        }
+
+        int[] result = new int[numCourses];
+        int index = 0;
+
+        while(!queue.isEmpty()) {
+            int curr = queue.poll();
+            result[index++] = curr;
+
+            for(int nei : graph.get(curr)) {
+                indegree[nei]--;
+                if(indegree[nei] == 0) {
+                    queue.add(nei);
+                }
+            }
+        }
+
+        if(index == numCourses) {
+            return result;
+        }
+
+        return new int[0];
+    }
+}
